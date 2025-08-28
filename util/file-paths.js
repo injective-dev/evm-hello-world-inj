8449862ced9587213abbc3be28f73bdb29bdd8fb
Brendan Graetz <bguiz@users.noreply.github.com>
2025-08-28 16:14:24 +0800

    fix: correct parsing when stack contains non-'file://' stack elements, e.g. 'node:'
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- util/file-paths.js
+++ util/file-paths.js
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const FILE_PATHS = {
+    rootDir: path.resolve(__dirname, '../'),
     dotEnv: path.resolve(__dirname, '../.env'),
     configJson: path.resolve(__dirname, '../config.json'),
     packageJson: path.resolve(__dirname, '../package.json'),
